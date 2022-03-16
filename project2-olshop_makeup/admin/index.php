299a982ca19128da76dfd1ba695610be0699d5e3
joshuayordana <joshuayordana8@gmail.com>
2022-03-16 18:56:20 +0700

    backend

--- project2-olshop_makeup/admin/index.php
+++ project2-olshop_makeup/admin/index.php
@@ -1,6 +1,17 @@
 <?php
     require "sys/connect.php";
 
+    echo "<table border='1'>
+            <tr>
+                <th>ID</th>
+                <th>Foto</th>
+                <th>Nama Produk</th>
+                <th>Deskripsi</th>
+                <th>Kandungan</th>
+                <th>Cara Pakai</th>
+                <th>BPOM</th>
+            </tr>";
+
     $sql = "SELECT * FROM product";
     $result = $conn->query($sql);
 
@@ -16,9 +27,20 @@
             $kandungan = $row['kandungan'];
             $cara_pakai = $row['cara_pakai'];
             $bpom = $row['bpom'];
-            echo "<img src='$path' width='400'>";
-            echo "<h2>$nama</h2>";
+
+            //Display
+            echo "<tr>";
+            echo "<td>$id</td>";
+            echo "<td><img src='$path' width='200'></td>";
+            echo "<td><b>$nama</b></td>";
+            echo "<td style='white-space: pre-line'>$deskripsi</td>";
+            echo "<td style='white-space: pre-line'>$kandungan</td>";
+            echo "<td style='white-space: pre-line'>$cara_pakai</td>";
+            //echo "<td>$bpom</td>";
+            echo "</tr>";
+            
         }
     }
-?>
 
+    echo "</table>";
+?>
