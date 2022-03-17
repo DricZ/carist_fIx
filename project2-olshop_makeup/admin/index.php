9fa696bb3c017d333d1a7b2e9eedd83b04de9901
joshuayordana <joshuayordana8@gmail.com>
2022-03-17 21:47:43 +0700

    kasih link untuk back

--- project2-olshop_makeup/admin/index.php
+++ project2-olshop_makeup/admin/index.php
@@ -1,7 +1,7 @@
 <?php
     require "sys/connect.php";
 
-    echo "<a href='./add_product.php'></a>";
+    echo "<a href='./add_product.php'>Add Product</a>";
 
     echo "<table border='1'>
             <tr>
@@ -48,3 +48,16 @@
 
     
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Products</title>
+</head>
+<body>
+    
+</body>
+</html>
--- project2-olshop_makeup/admin/sys/upload_product.php
+++ project2-olshop_makeup/admin/sys/upload_product.php
@@ -28,9 +28,9 @@
     }
 
     // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
+    //if (file_exists($target_file)) {
+    //    echo "Sorry, file already exists.";
+    //    $uploadOk = 0;
     }
 
     // Check file size
