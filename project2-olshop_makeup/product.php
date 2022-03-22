182f82ed93b3148e539f815aa1b962c549d7d51a
joshuayordana <joshuayordana8@gmail.com>
2022-03-22 18:36:30 +0700

    Update product.php

--- project2-olshop_makeup/product.php
+++ project2-olshop_makeup/product.php
@@ -3,8 +3,50 @@
         require_once 'header.php';
         require "admin/sys/connect.php";
 
-        $sql = "SELECT * FROM product";
+        $sql = "SELECT * FROM product LIMIT 16";
         $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $id = $row['id'];
+                $relative = "../img/PRODUK/";
+                $img = $row['img'];
+                $path = $relative . $img;
+                $nama = $row['nama'];
+                $deskripsi = $row['deskripsi'];
+                $kandungan = $row['kandungan'];
+                $cara_pakai = $row['cara_pakai'];
+                $bpom = $row['bpom'];
+                $category = "";
+
+                //Search Category
+                $sql2 = "SELECT category.category_id AS category_id, category_list.nama FROM category LEFT JOIN category_list ON category.category_id = category_list.id WHERE product_id = '$id'";
+                $result2 = $conn->query($sql2);
+
+                if ($result2->num_rows > 0) {
+                    // output data of each row
+                    while($row2 = $result2->fetch_assoc()) {
+                        $temp = $row2['nama'];
+                        $category = $category . $temp . ", ";
+                    }
+                }
+                
+
+                //Display
+                echo "<tr>";
+                echo "<td>$id</td>";
+                echo "<td><img src='$path' width='200'></td>";
+                echo "<td><b>$nama</b></td>";
+                echo "<td style='white-space: pre-line'>$deskripsi</td>";
+                echo "<td style='white-space: pre-line'>$kandungan</td>";
+                echo "<td style='white-space: pre-line'>$cara_pakai</td>";
+                echo "<td>$bpom</td>";
+                echo "<td>$category</td>";
+                echo "</tr>";
+                
+            }
+        }
 ?>
 
 <!DOCTYPE html>
