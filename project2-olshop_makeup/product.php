74f69fb7f4f3bf4dff6382c553ae6a2ff3257c6b
joshuayordana <joshuayordana8@gmail.com>
2022-03-22 20:46:55 +0700

    Update product.php

--- project2-olshop_makeup/product.php
+++ project2-olshop_makeup/product.php
@@ -2,8 +2,13 @@
         require_once 'include.php';
         require_once 'header.php';
         require "admin/sys/connect.php";
+        
 
         $sql = "SELECT * FROM product LIMIT 16";
+        if(isset($_GET['filter'])){
+            $filter = $_GET['filter'];
+            echo "    Category: ".$filter;
+        }
         $result = $conn->query($sql);
 
         
@@ -42,70 +47,70 @@
 
                 <a onclick="openbar('bar1')" class="">Based on PRODUCT HIGHLIGHT</a>
                 <div id="bar1" hidden>
-                    <a class="dalem" onclick="">Best Seller / Recomendation</a>
-                    <a class="dalem" onclick="">New Arrival</a>
+                    <a class="dalem" href="?filter=Best Seller / Recomendation">Best Seller / Recomendation</a>
+                    <a class="dalem" href="?filter=New Arrival">New Arrival</a>
                 </div>
 
                 <a onclick="openbar('bar2')" class="">Based on SKIN TYPE</a>
                 <div id="bar2" hidden>
-                    <a class="dalem" onclick="">All Skin Type</a>
-                    <a class="dalem" onclick="">Normal</a>
-                    <a class="dalem" onclick="">Dry Skin</a>
-                    <a class="dalem" onclick="">Oily Skin</a>
-                    <a class="dalem" onclick="">Sensitive Skin</a>
+                    <a class="dalem" href="?filter=All Skin Type">All Skin Type</a>
+                    <a class="dalem" href="?filter=Normal">Normal</a>
+                    <a class="dalem" href="?filter=Dry Skin">Dry Skin</a>
+                    <a class="dalem" href="?filter=Oily Skin">Oily Skin</a>
+                    <a class="dalem" href="?filter=Sensitive Skin">Sensitive Skin</a>
                 </div>
 
                 <a onclick="openbar('bar3')" class="">Based on SKIN CONDITION</a>
                 <div id="bar3" hidden>
-                    <a class="dalem" onclick="">Normal</a>
-                    <a class="dalem" onclick="">Kusam</a>
-                    <a class="dalem" onclick="">Berjerawat</a>
-                    <a class="dalem" onclick="">Aging</a>
-                    <a class="dalem" onclick="">Kering</a>
-                    <a class="dalem" onclick="">Inflamasi</a>
+                    <a class="dalem" href="?filter=Normal">Normal</a>
+                    <a class="dalem" href="?filter=Kusam">Kusam</a>
+                    <a class="dalem" href="?filter=Berjerawat">Berjerawat</a>
+                    <a class="dalem" href="?filter=Aging">Aging</a>
+                    <a class="dalem" href="?filter=Kering">Kering</a>
+                    <a class="dalem" href="?filter=Inflamasi">Inflamasi</a>
                 </div>
 
                 <a onclick="openbar('bar4')" class="">Based on ACNE SEVERITY</a>
                 <div id="bar4" hidden>
-                    <a class="dalem" onclick="">All Condition</a>
-                    <a class="dalem" onclick="">Komedo</a>
-                    <a class="dalem" onclick="">Papule (light)</a>
-                    <a class="dalem" onclick="">Pustule (Medium)</a>
-                    <a class="dalem" onclick="">Nodule ( Severe)</a>
+                    <a class="dalem" href="?filter=All Condition">All Condition</a>
+                    <a class="dalem" href="?filter=Komedo">Komedo</a>
+                    <a class="dalem" href="?filter=Papule (light)">Papule (light)</a>
+                    <a class="dalem" href="?filter=Pustule (Medium)">Pustule (Medium)</a>
+                    <a class="dalem" href="?filter=Nodule (Severe)">Nodule (Severe)</a>
                 </div>
 
                 <a onclick="openbar('bar5')" class="">Based on PRODUCT TYPE</a>
                 <div id="bar5" hidden>
-                    <a class="dalem" onclick="">Face Wash</a>
-                    <a class="dalem" onclick="">Face Cleanser</a>
-                    <a class="dalem" onclick="">Face Toner</a>
-                    <a class="dalem" onclick="">Serum</a>
-                    <a class="dalem" onclick="">Cream</a>
-                    <a class="dalem" onclick="">Gel</a>
-                    <a class="dalem" onclick="">Lotion</a>
-                    <a class="dalem" onclick="">Body Care</a>
-                    <a class="dalem" onclick="">Neutralizing</a>
-                    <a class="dalem" onclick="">Peeling</a>
-                    <a class="dalem" onclick="">Decorative</a>
+                    <a class="dalem" href="?filter=Face Wash">Face Wash</a>
+                    <a class="dalem" href="?filter=Face Cleanser">Face Cleanser</a>
+                    <a class="dalem" href="?filter=Face Toner">Face Toner</a>
+                    <a class="dalem" href="?filter=Serum">Serum</a>
+                    <a class="dalem" href="?filter=Cream">Cream</a>
+                    <a class="dalem" href="?filter=Gel">Gel</a>
+                    <a class="dalem" href="?filter=Lotion">Lotion</a>
+                    <a class="dalem" href="?filter=Body Care">Body Care</a>
+                    <a class="dalem" href="?filter=Neutralizing">Neutralizing</a>
+                    <a class="dalem" href="?filter=Peeling">Peeling</a>
+                    <a class="dalem" href="?filter=Decorative">Decorative</a>
                 </div>
 
                 <a onclick="openbar('bar6')" class="">Based on FUNCTION</a>
                 <div id="bar6" hidden>
-                    <a class="dalem" onclick="">Sunscreen</a>
-                    <a class="dalem" onclick="">Moisturizer</a>
-                    <a class="dalem" onclick="">Brightening</a>
-                    <a class="dalem" onclick="">Acne Care</a>
-                    <a class="dalem" onclick="">Anti Aging</a>
-                    <a class="dalem" onclick="">Nutritive</a>
-                    <a class="dalem" onclick="">Conditioning</a>
+                    <a class="dalem" href="?filter=Sunscreen">Sunscreen</a>
+                    <a class="dalem" href="?filter=Moisturizer">Moisturizer</a>
+                    <a class="dalem" href="?filter=Brightening">Brightening</a>
+                    <a class="dalem" href="?filter=Acne Care">Acne Care</a>
+                    <a class="dalem" href="?filter=Anti Aging">Anti Aging</a>
+                    <a class="dalem" href="?filter=Nutritive">Nutritive</a>
+                    <a class="dalem" href="?filter=Conditioning">Conditioning</a>
                 </div>
 
                 <a onclick="openbar('bar7')" class="">Based on BRAND</a>
                 <div id="bar7" hidden>
-                    <a class="dalem" onclick="">Alfacid</a>
-                    <a class="dalem" onclick="">Primaderma</a>
-                    <a class="dalem" onclick="">Hydrosnail</a>
-                    <a class="dalem" onclick="">Solasense</a>
+                    <a class="dalem" href="?filter=Alfacid">Alfacid</a>
+                    <a class="dalem" href="?filter=Primaderma">Primaderma</a>
+                    <a class="dalem" href="?filter=Hydrosnail">Hydrosnail</a>
+                    <a class="dalem" href="?filter=Solasense">Solasense</a>
                 </div>
             </div>
 
@@ -145,6 +150,7 @@
                     <div class="card produk-p" onclick="openProduk('<?=$nama?>')">
                         <img src="<?=$path?>" alt="<?=$img?>">
                         <center class="judul pr"><?=$nama?></center>
+                        <center><p><i>Best Seller, Alfacid</i></p></center>
                     </div>
                 </div>
                 <?php
