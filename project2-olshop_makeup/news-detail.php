01374b00fc5455f6f30737e95654ef2c54729cc1
DricZ <drcsm2013@gmail.com>
2022-03-23 21:23:45 +0700

    update news detail

---
+++ project2-olshop_makeup/news-detail.php
@@ -0,0 +1,137 @@
+<?php
+        require_once 'include.php';
+        require_once 'header.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>UFI</title>
+
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <link href="./css/style.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <hr style="margin-bottom: 0;" />
+        <div class="container-fluid megathronz-news" style="background-image: url('./img/gp.jpg'); ">   
+        </div>
+        <p><i style="margin: 20px; opacity: 0.8">Picture Credit/description</i></p>
+        
+        
+        <div class="container">
+            <center>
+                <h2><b>News Headline Here: Put Here</b></h2><br>
+                <h4><b>Subtitle Here: Put Here<b></h4><br><br>
+                <p>
+                    <b>By Author Name</b><br>
+                </p>
+                <h5><b>January 1, 2022<b></h5><br><br>
+            </center>
+
+            <p style="text-align:justify; width: 50vw">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed velit at leo tempus tempus. Vestibulum eros augue, facilisis nec urna sed, gravida posuere lacus. Integer nec sem a lectus ultrices hendrerit vitae at leo. Nullam scelerisque elit at dolor pulvinar, id congue nibh pretium. Proin in suscipit orci, et condimentum ipsum. Phasellus maximus luctus nulla, nec rutrum eros maximus condimentum. Nulla sit amet volutpat sapien. Nullam tristique eu sem cursus aliquet. Vivamus malesuada a augue nec sollicitudin.<br>
+
+                Etiam suscipit neque vel elementum accumsan. Donec aliquet, dui sit amet egestas blandit, eros est convallis tortor, ut pharetra eros ante vel felis. Sed felis augue, pellentesque sed commodo a, sollicitudin id tellus. Donec blandit bibendum risus, vitae rutrum enim auctor sed. Duis pellentesque semper est, interdum malesuada orci luctus vel. Duis ut mi pellentesque, sagittis risus vel, faucibus massa. Etiam urna velit, tincidunt sed interdum tincidunt, vulputate at erat. Proin in fermentum nunc. Suspendisse pharetra dolor eget ligula venenatis, aliquet sodales risus tristique. Cras pellentesque odio convallis augue sodales, sit amet lobortis dui aliquam. Integer ornare, nisl id cursus convallis, felis odio volutpat ante, sit amet consequat erat massa ac erat.<br>
+
+                Etiam ut hendrerit libero. Proin in metus dictum, elementum nunc eget, tincidunt nunc. Vestibulum a dictum tortor. Ut volutpat in urna eu vehicula. Suspendisse ac vulputate orci. Proin eleifend commodo lectus. Sed et lacus pellentesque, vestibulum orci non, congue arcu. Nunc elementum quam eu tortor ullamcorper iaculis. Nam felis mi, feugiat eget sem a, malesuada congue neque. Nulla ac turpis tortor. Pellentesque et urna ut magna convallis venenatis nec vitae turpis. Sed ultrices vehicula maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec eu ornare libero. In convallis, libero vitae rutrum dictum, dui massa lacinia nisl, at viverra odio augue id ex. Vivamus ex libero, feugiat sit amet quam nec, efficitur accumsan ipsum.<br>
+
+                Sed sed efficitur enim, non fermentum mauris. Fusce rhoncus arcu nec dolor porta blandit nec a purus. Nunc turpis ligula, mollis et finibus a, pellentesque eget lacus. Praesent dapibus tortor dui, sit amet molestie tellus posuere sed. Sed quis sem auctor, efficitur augue quis, bibendum nisl. Maecenas ut accumsan dui. Vivamus iaculis posuere hendrerit. Vivamus sed vehicula massa. Suspendisse facilisis in risus vel rutrum.<br>
+
+                Duis massa felis, sodales non lectus id, posuere mollis ipsum. Mauris dolor est, consequat vel dui et, scelerisque vehicula mi. Fusce tincidunt arcu sed ante convallis, id venenatis nunc hendrerit. Maecenas semper turpis ac tempor imperdiet. Vivamus scelerisque erat ut sem tempor, sit amet tincidunt dolor tempus. Vestibulum varius lobortis turpis, pellentesque hendrerit velit imperdiet vel. Phasellus pharetra, tortor eu vehicula molestie, magna tortor accumsan tellus, eu vulputate quam lectus quis dolor. Nullam hendrerit elit in tortor fermentum, sed consectetur felis eleifend. Nullam ac justo in arcu pulvinar dapibus a non tellus. Donec hendrerit tincidunt tortor ac aliquam. Nullam eu sollicitudin arcu, eget aliquet nisl.
+            </p>
+        </div>
+        
+        <hr/>
+
+        <div class="container">
+            <center>
+                <h5><b>Read More</b></h5>
+                
+                <div class="row">
+
+                    <!-- Carousel -->
+                    <div id="products" class="carousel slide" data-bs-ride="carousel">
+
+                    <!-- The slideshow/carousel -->
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div class="row justify-content-center">
+                                    <div class="col-4 card p ">
+                                        <img class="card-img-top product" src="./img/PRODUK/Alfacid AHA _ BHA pore serum.jpg" alt="Fasilitas">
+                                        <center class="judul pr">Nama Produk</center>
+                                    </div>
+
+                                    <div class="col-4 card p">
+                                        <img class="card-img-top product" src="./img/PRODUK/Alfacid Face Toner.jpg" alt="Fasilitas">
+                                        <center class="judul pr">Nama Produk</center>
+                                    </div>
+
+                                    <div class="col-4 card p">
+                                        <img class="card-img-top product" src="./img/PRODUK/Alfacid Face Toner.jpg" alt="Fasilitas">
+                                        <center class="judul pr">Nama Produk</center>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row justify-content-center">
+                                    <div class="col-4 card p ">
+                                        <img class="card-img-top product" src="./img/PRODUK/Alfacid AHA _ BHA pore serum.jpg" alt="Fasilitas">
+                                        <center class="judul pr">Nama Produk</center>
+                                    </div>
+
+                                    <div class="col-4 card p">
+                                        <img class="card-img-top product" src="./img/PRODUK/Alfacid Face Toner.jpg" alt="Fasilitas">
+                                        <center class="judul pr">Nama Produk</center>
+                                    </div>
+
+                                    <div class="col-4 card p">
+                                        <img class="card-img-top product" src="./img/PRODUK/Alfacid Face Toner.jpg" alt="Fasilitas">
+                                        <center class="judul pr">Nama Produk</center>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row justify-content-center">
+                                    <div class="col-4 card p ">
+                                        <img class="card-img-top product" src="./img/PRODUK/Alfacid AHA _ BHA pore serum.jpg" alt="Fasilitas">
+                                        <center class="judul pr">Nama Produk</center>
+                                    </div>
+
+                                    <div class="col-4 card p">
+                                        <img class="card-img-top product" src="./img/PRODUK/Alfacid Face Toner.jpg" alt="Fasilitas">
+                                        <center class="judul pr">Nama Produk</center>
+                                    </div>
+
+                                    <div class="col-4 card p">
+                                        <img class="card-img-top product" src="./img/PRODUK/Alfacid Face Toner.jpg" alt="Fasilitas">
+                                        <center class="judul pr">Nama Produk</center>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <center>
+                        <button type="button" class="button-active btn-2" value="SHOP" onclick="">VIEW ALL PRODUCTS</button>
+
+                    </center>
+                </div>
+                
+            </div>
+            </center>
+            
+        </div>
+        
+    </body>
+
+    <!-- FOOTER -->
+    <?php
+    include 'footer.php';
+    ?>
+</html>
