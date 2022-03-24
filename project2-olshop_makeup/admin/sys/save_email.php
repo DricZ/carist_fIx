0ba9957e799d944abebc005b512fd92b7ecf4cf2
joshuayordana <joshuayordana8@gmail.com>
2022-03-24 15:01:05 +0700

    news detail

--- project2-olshop_makeup/admin/sys/save_email.php
+++ project2-olshop_makeup/admin/sys/save_email.php
@@ -6,46 +6,25 @@
     //Masukkan ke DB
     $sql = "INSERT INTO email (email)
             VALUES ('$email')";
-    //$result = $conn->query($sql);
+    $result = $conn->query($sql);
 
-    // if ($result === TRUE) { //Jika input client ke Database Sukses
-    //     echo "<br>Save Email Sukses!<br>";
-    //     //echo "<a href='../add_product.php'>Back</a>";
-    //     header("Location: ../../index.php");
-    // }
+    if ($result === TRUE) { //Jika input client ke Database Sukses
+        echo "<br>Save Email Sukses!<br>";
+        //echo "<a href='../add_product.php'>Back</a>";
+        
+        ini_set( 'display_errors', 1 );   
+        error_reporting( E_ALL );    
+        $from = "system@unitedfarmaticindonesia.com";    
+        $to = "$email";
+        $subject = "Terimakasih telah subscribe";
+        $message = "Email anda $email telah terdaftar di server kami, terimakasih";
+        $headers = "From:" . $from;
+        $headers .= 'Cc: joshuayordana8@gmail.com' . "\r\n";
+        mail($to,$subject,$message, $headers);
+        echo "Pesan email sudah terkirim.";
+    }
 
-    $to = "joshuayordana8@gmail.com";
-    $subject = "Test email";
+    header("Location: ../../product.php");
     
-    $message = "
-    <html>
-    <head>
-    <title>HTML email</title>
-    </head>
-    <body>
-    <p>This email contains HTML Tags!</p>
-    <table>
-    <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    </tr>
-    <tr>
-    <td>John</td>
-    <td>Doe</td>
-    </tr>
-    </table>
-    </body>
-    </html>
-    ";
-    
-    // Always set content-type when sending HTML email
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    
-    // More headers
-    $headers .= 'From: <webmaster@example.com>' . "\r\n";
-    $headers .= 'Cc: myboss@example.com' . "\r\n";
-    
-   mail($to,$subject,$message,$headers);
 
 ?>
