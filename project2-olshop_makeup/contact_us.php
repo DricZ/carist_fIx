f106cc4c40b0229de70d8824add33cbf2407743a
DricZ <drcsm2013@gmail.com>
2022-03-26 11:30:32 +0700

    update

--- project2-olshop_makeup/contact_us.php
+++ project2-olshop_makeup/contact_us.php
@@ -34,17 +34,26 @@
                         </ul>
                     </div>    
                 </div>
-                <div class="col find-us" style="margin-left: 5vw;">
+                <div class="col find-us d-index" style="margin-left: 5vw;">
                     <a onclick="window.location.href='https://wa.me/6281328367388'"><img src="./img/wa.png" style="width: 170%;
     margin: -8vw; margin-bottom: -18vw;"></a>
                 </div>
+
+                <div class="col center m-index" style="height: 25vh; display: flex;">
+                    <a onclick="window.location.href='https://wa.me/6281328367388'"><img src="./img/wa.png" style="width: 100%;"></a>
+                </div>
+
                 <div class="col">
                     <h5 style="margin-bottom: 20px;">Find Us on Our Socials</h5>
                     <!-- <div class="row find-us">
                         <button type="button" class="btn btn-2" onclick="window.location.href='https://www.instagram.com/ufi.indonesia/'"><b>Youtube</b></button>
                     </div> -->
 
-                    <div class="row find-us">
+                    <div class="row find-us d-index">
+                        <button type="button" class="btn btn-2" onclick="window.location.href='https://www.instagram.com/ufi.indonesia/'"><b>Instagram</b></button>
+                    </div>
+
+                    <div class="row find-us center m-index" style="display:flex">
                         <button type="button" class="btn btn-2" onclick="window.location.href='https://www.instagram.com/ufi.indonesia/'"><b>Instagram</b></button>
                     </div>
 
