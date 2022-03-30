066db43fd7cad14da6def2e3aa95f60811a2341d
DricZ <drcsm2013@gmail.com>
2022-03-30 14:13:25 +0700

    Update faq.php

--- project2-olshop_makeup/faq.php
+++ project2-olshop_makeup/faq.php
@@ -12,6 +12,11 @@
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <link href="./css/style.css" rel="stylesheet">
 
+        <style>
+            html,body {
+                height: 100%;
+            }
+        </style>
     </head>
 
     <body>
