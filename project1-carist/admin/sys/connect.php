388ca3f98d5322ecc911c816f1f6cb15f2e637b0
joshuayordana <joshuayordana8@gmail.com>
2022-04-04 20:57:26 +0700

    Update connect.php

--- project1-carist/admin/sys/connect.php
+++ project1-carist/admin/sys/connect.php
@@ -1,13 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "carist_panel";
+  // $servername = "localhost";
+  // $username = "root";
+  // $password = "";
+  // $dbname = "carist_panel";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+  $servername = "srv25.niagahoster.com";
+  $username = "u1653205_admin";
+  $password = "admincarist";
+  $dbname = "u1653205_carist_panel";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 ?>
