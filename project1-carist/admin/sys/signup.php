46588277af30e74bc6c1e9c16336d7127555a5a7
joshuayordana <joshuayordana8@gmail.com>
2022-04-04 23:40:25 +0700

    Create signup.php

---
+++ project1-carist/admin/sys/signup.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    require "connect.php";
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $name = $_POST["name"];
+    // $email = $_POST["email"];
+    // $phone = $_POST["phone"];
+    // $city = $_POST["city"];
+    // $address = $_POST["address"];
+    // $birthday = $_POST["birthday"];
+    // $instagram = $_POST["instagram"];
+    // $tiktok = $_POST["tiktok"];
+
+    //Input ke Database
+    $sql = "INSERT INTO user (username, pass, name, status)
+    VALUES ($username, $password, 'pending')";
+    if ($conn->query($sql) === TRUE) {
+        //echo "New record created successfully";
+        //Redirect Back
+        header("Location: ../");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        die();
+    }
+    $conn->close();
+    die();
+?>
