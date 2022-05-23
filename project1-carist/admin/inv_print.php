19680805cc13cc938ad425dd79618f25faa8699e
DricZ <drcsm2013@gmail.com>
2022-05-23 08:31:26 +0700

    Up inv print

--- project1-carist/admin/inv_print.php
+++ project1-carist/admin/inv_print.php
@@ -54,33 +54,13 @@
     <!-- Page Wrapper -->
     <div id="wrapper">
 
-        <!-- SIDEBAR -->
-        <?php
-            require 'sidebar.php';
-        ?>
-
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
 
             <!-- Main Content -->
             <div id="content">
 
-                <!-- Topbar -->
-                <?php
-                    require 'header.php';
-                ?>
-                <!-- End of Topbar -->
-
-                <?php
-                    require 'sidebar_button.php';
-                ?>
-
                 <!-- Begin Page Content -->
-                <nav style="margin-bottom: 30px;margin-left: 40px;" class="breadcrumbs">
-                    <a href="./new_client.php?id=<?=$client_id?>" class="breadcrumbs__item">Brand Information</a> 
-                    <a href="#" class="breadcrumbs__item is-active">Invoice</a> 
-                </nav>
-
                 <div class="row" style="padding-left: 40px;padding-right: 40px;">
                     <div class="col" style="padding-left: 30px;">
                         <div class="row">
@@ -177,20 +157,6 @@
                         </div>
                     </div>
 
-                    <div class="row mb-5" style="display: grid; justify-content: right;">
-                        <div class="col">
-                            <button class="btn btn-primary mr-5" onclick="printFunction()">PRINT</button>
-                            <script>
-                                function printFunction() {
-                                    $("<iframe>")                             
-                                        .hide()                               
-                                        .attr("src", "./inv_print.php?id=<?=$client_id?>") 
-                                        .appendTo("body");                    
-                                }
-                            </script>
-                            <button class="btn btn-primary" onclick="window.location.href='./invoice_email.php?id=<?=$client_id?>'">SEND</button>
-                        </div>
-                    </div>
                 </div>
                 <!-- /.container-fluid -->
 
@@ -209,31 +175,6 @@
     </div>
     <!-- End of Page Wrapper -->
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="logout.php">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -250,6 +191,12 @@
     <!-- Page level custom scripts -->
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            window.print();
+        });
+    </script>
 
 </body>
 
